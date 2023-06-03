268ebd190ee761d018d041bef4ac064dc31125d5
Ayaka Mikazuki <ayaka@mail.shn.hk>
2023-06-04 01:26:14 +0800

    First commit

---
+++ main.py
@@ -0,0 +1,80 @@
+import cherrypy
+import csv
+import sqlite3
+
+def 構建():
+    是方言的簡稱們 = set()
+    簡稱到分區與顏色 = {}
+    簡稱到排序 = {}
+
+    with open('data/info.csv', encoding='utf-8') as f:
+        reader = csv.reader(f, delimiter=',', quotechar='"', strict=True)
+
+        _, _, *rest = next(reader)
+        分區_idx = rest.index('地圖集二分區')
+        顏色_idx = rest.index('地圖集二顏色')
+        排序_idx = rest.index('地圖集二排序')
+
+        for _, 簡稱, *rest in reader:
+            分區 = rest[分區_idx]
+            顏色 = rest[顏色_idx]
+            排序 = rest[排序_idx]
+
+            if 分區 and 顏色 and 排序:
+                是方言的簡稱們.add(簡稱)
+                簡稱到分區與顏色[簡稱] = 分區, 顏色
+                簡稱到排序[簡稱] = 排序
+
+    return 是方言的簡稱們, 簡稱到分區與顏色, 簡稱到排序
+
+是方言的簡稱們, 簡稱到分區與顏色, 簡稱到排序 = 構建()
+
+class Server:
+    def __init__(self):
+        original_db = sqlite3.connect('file:data/mcpdict.db?mode=ro', uri=True)
+        db = sqlite3.connect(':memory:', check_same_thread=False)  # we are safe because we only use the database in a read-only manner
+        original_db.backup(db)
+        original_db.close()
+        self.db = db
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def query(self, string: str) -> dict:
+        if not string:
+            return []
+
+        cur = self.db.cursor()
+
+        string = list({漢字: None for 漢字 in string})  # 去重
+        placeholders = ', '.join('?' for _ in string)
+        cur.execute(f'SELECT * FROM "mcpdict" WHERE "漢字" IN ({placeholders});', string)
+
+        results = cur.fetchall()
+        column_names = [description[0] for description in cur.description]
+
+        cur.close()
+
+        漢字到idx = {漢字: idx for idx, 漢字 in enumerate(string)}
+
+        漢字_column = column_names.index('漢字')
+        results = sorted(results, key=lambda result: 漢字到idx[result[漢字_column]])
+        字頭們 = [result[漢字_column] for result in results]
+
+        return [('', *字頭們)] + sorted(
+            ((
+                簡稱, *簡稱到分區與顏色[簡稱], *字音們)
+                for 簡稱, *字音們
+                in zip(column_names, *results)
+                if 簡稱 in 是方言的簡稱們 and all(字音 for 字音 in 字音們)
+            ),
+            key=lambda 簡稱_rest: 簡稱到排序[簡稱_rest[0]]
+        )
+
+if __name__ == '__main__':
+    cherrypy.config.update({
+        'environment': 'production',
+        'log.screen': False,
+        'server.socket_host': '0.0.0.0',
+        'show_tracebacks': False,
+    })
+    cherrypy.quickstart(Server())
