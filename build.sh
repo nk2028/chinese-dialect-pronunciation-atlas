20309272bbf4172a05a62c0c9f64713c0df89fd7
untunt <unt_tianjin@163.com>
2024-11-25 10:09:36 +0800

    Change data source fork back to osfans

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-git clone --depth=1 https://github.com/nk2028/MCPDict.git
+git clone --depth=1 https://github.com/osfans/MCPDict.git
 cd MCPDict/tools/
+# Mark all dialects as new to force building all of them
+sed -i 's/def outdated():/def outdated():\n\treturn False/g' tables/_詳情.py
 python make.py
 cd ../../
 mkdir -p data/
